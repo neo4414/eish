a744cda67e0c701f135ccab7d940dccb4dbba62f
neo <dineomay30@gmail.com>
2024-07-17 03:41:00 -0700

    i love coding

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,13 @@
-
+import react from 'react';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
   return (
     <>
+    <div className='App'>
+      <Counter />
+    </div>
     <div>
       <h1>this is react</h1>
     </div>
